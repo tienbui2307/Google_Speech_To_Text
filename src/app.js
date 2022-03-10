984e4ac4b1104e57df4ad766fc4fa9673be3efe7
TienBui <tienbui237.550@gmail.com>
2022-03-10 15:18:38 +0700

    Remove host

--- src/app.js
+++ src/app.js
@@ -117,7 +117,7 @@ const request = {
 
 // =========================== START SERVER ================================ //
 
-server.listen(port, "127.0.0.1", function () {
+server.listen(port, function () {
   //http listen, to make socket work
   // app.address = "127.0.0.1";
   console.log("Server started on port:" + port);
